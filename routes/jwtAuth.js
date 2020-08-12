09f0b38265443bd05eb1546bfd07393a2a46b183
Vik Birdi <lifeasvik@gmail.com>
2020-08-12 15:39:12 -0500

    added login route

--- routes/jwtAuth.js
+++ routes/jwtAuth.js
@@ -4,6 +4,8 @@ const pool = require("../db")
 
 const bcrypt = require ("bcrypt");
 
+const jwtGenerator = require("../utilities/jwtGenerator");
+
 //register route
 
 router.post("/register", async (req, res) => {
@@ -37,6 +39,50 @@ router.post("/register", async (req, res) => {
         res.json(newUser.rows[0]);
 
         //5. generating our jwt token
+
+        const token = jwtGenerator(newUser.rows[0].user_id);
+
+        res.json({token})
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+//login route
+
+router.post("/login", async (req,res) => {
+    try {
+
+    //1. destructure req.body
+
+    const {email, password} = req.body;
+
+    //2. check user exists
+
+    const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
+        email
+    ]);
+
+    if(user.rows.length === 0){
+        return res.status(401).send("Password or Email is incorrect");
+    }
+
+    //3. check password against db
+
+    const validPassword = await bcrypt.compare(password, user.rows[0].user_password);
+
+    if(!validPassword){
+        return res.status(401).json("Password or Email is incorrect");
+    }
+
+    //4. give user jwt token
+
+    const token = jwtGenerator(user.rows[0].user_id);
+
+    res.json({token});
+
         
     } catch (err) {
         console.error(err.message);
